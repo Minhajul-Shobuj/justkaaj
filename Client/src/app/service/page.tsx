d9182ca67afbccb97de0792b3e84a6e9af7f690b
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-10-25 00:53:31 +0600

    updated service page

--- Client/src/app/service/page.tsx
+++ Client/src/app/service/page.tsx
@@ -81,16 +81,18 @@ const ServicePage = () => {
               </button>
             </div>
           </form>
-          if (services.length === 0){" "}
-          {
-            <div className="text-center text-gray-600 py-10 text-lg">
-              No services found.
-            </div>
-          }
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {services.map((service) => (
-              <ServiceCard key={service.id} service={service} />
-            ))}
+          <div>
+            {services.length === 0 ? (
+              <div className="text-center text-gray-600 py-10 text-lg">
+                No services found.
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {services.map((service) => (
+                  <ServiceCard key={service.id} service={service} />
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
